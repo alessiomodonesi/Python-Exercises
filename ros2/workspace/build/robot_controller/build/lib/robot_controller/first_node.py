f447e4b1e3e3ddd62d0b5066370ba4af98978e64
alessiomodonesi <alessiomodo2004@gmail.com>
2026-07-17 17:14:48 +0200

    create draw_circle

--- ros2/workspace/build/robot_controller/build/lib/robot_controller/first_node.py
+++ ros2/workspace/build/robot_controller/build/lib/robot_controller/first_node.py
@@ -7,7 +7,12 @@ class MyNode(Node):
     
     def __init__(self):
         super().__init__("first_node")
-        self.get_logger().info("Hello from ROS2")
+        self.counter_ = 0
+        self.create_timer(1.0, self.timer_callback)
+    
+    def timer_callback(self):
+        self.get_logger().info("Hello " + str(self.counter_))
+        self.counter_ += 1
 
 def main(args=None):
     # initialize ros2 communications
